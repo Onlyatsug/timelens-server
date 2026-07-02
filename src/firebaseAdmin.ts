46abefce522b58c1d3a237e34057b8c3802dc503
Onlyatsug <gsb.jmt@gmail.com>
2026-07-02 18:15:10 -0400

    feat: deploy fixes

--- src/firebaseAdmin.ts
+++ src/firebaseAdmin.ts
@@ -2,14 +2,31 @@
 import { initializeApp, cert, getApps } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 import { getFirestore } from 'firebase-admin/firestore'; // 🔥 Adicionado
+require("dotenv").config();
 
-const serviceAccount = require('./base/timelens.json');
+const privateKey = process.env.PRIVATE_KEY;
+if (!privateKey) {
+  throw new Error("PRIVATE_KEY não definida");
+}
+
+const serviceAccount: Object = {
+  type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+  universe_domain: process.env.UNIVERSE_DOMAIN,
+};
 
 if (!getApps().length) {
   initializeApp({
     credential: cert(serviceAccount),
   });
 }
-
 export const firebaseAuth = getAuth();
 export const db = getFirestore(); // 🔥 Exportando a instância do banco de dados
